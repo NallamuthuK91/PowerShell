e19906ca46b0d79442305668a7f1df5b89313694
NallamuthuK91 <98209715+NallamuthuK91@users.noreply.github.com>
2022-11-28 15:28:47 +0530

    CreateNewFile PS
    NewFile

---
+++ copyitem.ps1
@@ -0,0 +1,12 @@
+Start-Transcript -Path H:\PSTransciptlogs
+$Folder1path = "C:\Users\Nallamuthu\Downloads\Telegram Desktop\"
+$Folder2path = "H:\Telegram_Downloads_Backup"
+$Folder1 = Get-childitem -path $Folder1path -Recurse
+$Folder2 = Get-childitem -path $Folder2path -Recurse
+Compare-Object -ReferenceObject $folder1 -DifferenceObject $Folder2
+Compare-Object $Folder1 $Folder2 -Property Name, Length  | 
+Where-Object {$_.SideIndicator -eq "<="} | 
+ForEach-Object {
+            Copy-Item "$Folder1path\$($_.name)" -Destination "$Folder2path" -Force -Recurse
+}
+Stop-Transcript
