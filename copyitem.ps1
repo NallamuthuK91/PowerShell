4b43cda29b53ac5e6b7b7b3038395c10fd648c63
NallamuthuK91 <98209715+NallamuthuK91@users.noreply.github.com>
2022-11-28 15:55:28 +0530

    Update copyitem.ps1

--- copyitem.ps1
+++ copyitem.ps1
@@ -3,9 +3,9 @@ $Folder1path = "C:\Users\Nallamuthu\Downloads\Telegram Desktop\"
 $Folder2path = "H:\Telegram_Downloads_Backup\"
 $Folder1 = Get-childitem -path $Folder1path -Recurse
 $Folder2 = Get-childitem -path $Folder2path -Recurse
-#Compare-Object -ReferenceObject $folder1 -DifferenceObject $Folder2
-#Compare-Object $Folder1 $Folder2 -Property Name, Length  | 
-#Where-Object {$_.SideIndicator -eq "<="} | 
+Compare-Object -ReferenceObject $folder1 -DifferenceObject $Folder2
+Compare-Object $Folder1 $Folder2 -Property Name, Length  | 
+Where-Object {$_.SideIndicator -eq "<="} | 
 ForEach-Object {
             Copy-Item "$Folder1path\$($_.name)" -Destination "$Folder2path" -Force -Recurse
 }
